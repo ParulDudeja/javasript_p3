378a23470a5a12454d693a5c3d03bd8db9b927e9
Parul Dudeja <paruldudeja93@gmail.com>
2022-07-28 16:58:20 +0400

    axios api practice commit to get images from API

--- app.js
+++ app.js
@@ -15,12 +15,13 @@
 // axios.get("https://swapi.dev/api/people/1/").then((res) => {
 //   console.log("resonpese", res);
 // });
+
 const btn = document.getElementById("btn");
 const pJoke = document.getElementById("joke");
 const getDadJoke = async () => {
   try {
     const config = { headers: { Accept: "application/json" } };
-    const res = await axios.get("https://icanhazdadjoke.com", config);
+    const res = await axios.get(`https://icanhazdadjoke.com`, config);
     //   console.log(res); // to get object
     //   console.log(res.data); // to get data out of object
     //   console.log(res.data.joke); // will fetch joke content by key value pair, where key is joke, and reult is value
@@ -37,3 +38,28 @@ const addNewJoke = async () => {
   pJoke.append(newLi);
 };
 btn.addEventListener("click", addNewJoke);
+
+// TV show search result through API
+
+const formSearch = document.getElementById("tv-show-form");
+const inputForm = document.getElementById("tv-show-query");
+const btnSearch = document.getElementById("btn-search");
+
+formSearch.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const searchText = formSearch.elements.query.value;
+  const para = { params: { q: searchText } };
+  const response = await axios.get(`http://api.tvmaze.com/search/shows`, para);
+  showImages(response.data);
+  formSearch.elements.query.value = "";
+});
+
+const showImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const newImg = document.createElement("img");
+      newImg.src = result.show.image.medium;
+      document.body.append(newImg);
+    }
+  }
+};
