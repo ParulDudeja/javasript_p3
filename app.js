e2c4110d29c0a593f2f09613478f368d81b55143
Parul Dudeja <paruldudeja93@gmail.com>
2022-07-28 14:21:18 +0400

    axios api practice commit

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
+// fetch API Practice
+// fetch("https://swapi.dev/api/people/1")
+//   .then((res) => {
+//     console.log("success");
+//     res.json().then((data) => {
+//       console.log("data" + data.name);
+//     });
+//   })
+//   .catch((e) => {
+//     console.log("error" + e);
+//   });
+
+//Axios practice
+
+// axios.get("https://swapi.dev/api/people/1/").then((res) => {
+//   console.log("resonpese", res);
+// });
 const btn = document.getElementById("btn");
-btn.addEventListener("click", () => {
-  console.log("clicked");
-});
+const pJoke = document.getElementById("joke");
+const getDadJoke = async () => {
+  try {
+    const config = { headers: { Accept: "application/json" } };
+    const res = await axios.get("https://icanhazdadjoke.com", config);
+    //   console.log(res); // to get object
+    //   console.log(res.data); // to get data out of object
+    //   console.log(res.data.joke); // will fetch joke content by key value pair, where key is joke, and reult is value
+    return res.data.joke;
+  } catch (e) {
+    return "No Joke available";
+  }
+};
+
+const addNewJoke = async () => {
+  const newJoke = await getDadJoke();
+  const newLi = document.createElement("Li");
+  newLi.append(newJoke);
+  pJoke.append(newLi);
+};
+btn.addEventListener("click", addNewJoke);
